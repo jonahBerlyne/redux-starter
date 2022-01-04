49716d00eb17fa887b80a6b36f786ad252c8205a
jonahBerlyne <jonshaber@gmail.com>
2022-01-04 13:41:01 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+
+const unsubscribe = store.subscribe(() => {
+ console.log("Store changed!", store.getState());
+});
+
+store.dispatch(bugAdded("Bug 1"));
+
+//unsubscribe();
+
+store.dispatch(bugResolved(1));
+store.dispatch(bugRemoved(1));
+console.log(store.getState());
